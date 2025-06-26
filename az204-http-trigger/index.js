ebd43b667320e2f03bd947fd93ad74a78619f30f
zearkiatos <caprilespe@outlook.com>
2025-06-25 21:20:28 -0400

    Adding azure function with blob storage output

--- az204-http-trigger/index.js
+++ az204-http-trigger/index.js
@@ -1,11 +1,11 @@
-﻿module.exports = async function (context, req) {
+module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    context.bindings.outputBlob = name;
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
